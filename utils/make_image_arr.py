e2f85519d01bc9cf858d8e253ccd7e19d6791d65
Jake W. Ireland <jakewilliami@icloud.com>
2021-02-04 11:23:50 +1300

    Updated python script to work with directory and data refactor.  Now allows target type to be command line arg

--- utils/make_image_arr.py
+++ utils/make_image_arr.py
@@ -1,30 +1,62 @@
 from PIL import Image # this is used to create the images
 import os # this is for reading paths
-from glob import glob
-from pathlib import Path # as above
+import pathlib # as above
+import sys # for command line arguments
 import random # this is used to choose 25 unique random images
 from math import ceil, floor
 
-# data_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "data")
-# data_dir = os.path.join(os.path.dirname(os.path.realpath(os.path.dirname(os.path.realpath(__file__)))), "data")
-data_dir = os.path.join(Path(os.path.realpath(__file__)).parents[1], "data")
+# --------------------------------------------------------
+# CHANGE THIS SECTION
+# --------------------------------------------------------
 
-print(data_dir)
+target_type = None # can be 'F', 'D', 'C', 'HF', or 'LF'.  Leaving it as `None` will choose a random one
+padding = 5
+arr_height, arr_width = (530, 530)
+nrows, ncols = (5, 5)
 
-# distractors = [os.path.abspath(i) for i in os.listdir(os.path.join(data_dir, "Distractor"))] # this is where you need to put your path to directory
-distractors glob.glob(os.path.join(data_dir, "Distractor"))
-print(distractors)
-cars = [os.path.abspath(i) for i in os.listdir(os.path.join(data_dir, "Cars"))]
-faces = [os.path.abspath(i) for i in os.listdir(os.path.join(data_dir, "Faces"))]
-hf_pareidolia = [os.path.abspath(i) for i in os.listdir(os.path.join(data_dir, "Highface pareidolia"))]
-lf_pareidolia = [os.path.abspath(i) for i in os.listdir(os.path.join(data_dir, "Lowface pareidolia"))]
+# --------------------------------------------------------
 
-randtarget = random.choice(cars + faces + hf_pareidolia + lf_pareidolia)
+# get target_type from input if required
+if len(sys.argv) > 1:
+    target_type = sys.argv[1]
+
+# get data directory
+data_dir = os.path.join(pathlib.Path(os.path.realpath(__file__)).parents[1], "data")
+
+# list directory with absolute paths
+def ls_abs(dirname, prefix):
+    dirname_dir = os.path.abspath(os.path.join(prefix, dirname))
+    ls_dir = os.listdir(dirname_dir)
+    return [os.path.abspath(os.path.join(dirname_dir, i)) for i in ls_dir]
+
+# list data directories
+distractors = ls_abs("Distractor", data_dir)
+cars = ls_abs("Cars", data_dir)
+faces = ls_abs("Faces", data_dir)
+hf_pareidolia = ls_abs("Highface pareidolia", data_dir)
+lf_pareidolia = ls_abs("Lowface pareidolia", data_dir)
+
+target_dict = {
+    'D': distractors,
+    'F': faces,
+    'C': cars,
+    'HF': hf_pareidolia,
+    'LF': lf_pareidolia
+}
+
 listofimages = random.sample(distractors, 24)
-listofimages.append(randtarget)
-listofimages = random.sample(listofimages, len(listofimages))
 
-print(listofimages)
+if target_type in target_dict:
+    if target_type != 'D':
+        randtarget = random.choice(target_dict[target_type])
+        listofimages.append(randtarget)
+        listofimages = random.sample(listofimages, len(listofimages))
+    else:
+        listofimages = random.sample(distractors, 25)
+else:
+    randtarget = random.choice(cars + faces + hf_pareidolia + lf_pareidolia)
+    listofimages.append(randtarget)
+    listofimages = random.sample(listofimages, len(listofimages))
 
 def create_collage_with_outer_borders(cols, rows, img_width, img_height, padding, listofimages):
     width = (img_width * cols) + (padding * (cols - 1))
@@ -88,16 +120,6 @@ def create_collage_resize(images_per_row, img_width, img_height, desired_frame_w
     return im.resize((desired_frame_width, desired_frame_height))
 
 img_width, img_height = Image.open(listofimages[0]).size # get size of images (assume all same size)
-
-# ---------------------------------
-# CHANGE THIS SECTION
-# ---------------------------------
-
-padding = 5
-arr_height, arr_width = (530, 530)
-nrows, ncols = (5, 5)
-
-# ---------------------------------
 
 wo_borders = create_collage_with_outer_borders(nrows, ncols, img_width, img_height, padding, listofimages)
 img = create_collage(nrows, img_width, img_height, padding, listofimages)
