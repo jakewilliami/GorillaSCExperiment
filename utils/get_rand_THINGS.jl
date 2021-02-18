78782a1bf2cd0d25524ec4ecdd44fbc6eb16dc0c
Jake W. Ireland <jakewilliami@icloud.com>
2021-02-18 13:33:45 +1300

    Updated random images from THINGS script

--- utils/get_rand_THINGS.jl
+++ utils/get_rand_THINGS.jl
@@ -1,14 +1,26 @@
-function main()
-    selected_images = collect(Base.Iterators.flatten([files for (_, _, files) in walkdir("/Users/jakeireland/projects/GorillaSCExperiment/data/chosen_object_images_1/")]))
+function get_files(dir::String)
+    return collect(Base.Iterators.flatten([files for (_, _, files) in walkdir(dir)]))
+end
+
+function get_root_and_files(dir::AbstractString)
     allfiles = Matrix{Int}(undef, 0, 2)
 
-    allfilesraw = [(root, files) for (root, _, files) in walkdir("/Users/jakeireland/projects/GorillaSCExperiment/data/THINGS/")]
+    allfilesraw = [(root, files) for (root, _, files) in walkdir(dir)]
 
     for (objectdir, objects) in allfilesraw
         for object in objects
             allfiles = cat(allfiles, [objectdir object], dims = 1)
         end
     end
+    
+    return allfiles
+end
+
+function main()
+    # selected_images = []
+    # selected_images = get_files("/Users/jakeireland/projects/GorillaSCExperiment/data/chosen_object_images_1/")
+    selected_images = vcat(get_files("/Users/jakeireland/projects/GorillaSCExperiment/data/chosen_object_images_1/"), get_files("/Users/jakeireland/projects/GorillaSCExperiment/data/chosen_object_images_2/"), get_files("/Users/jakeireland/projects/GorillaSCExperiment/data/chosen_object_images_3/"))
+    allfiles = get_root_and_files("/Users/jakeireland/projects/GorillaSCExperiment/data/THINGS/")
 
     i = 0
     while i < 200
@@ -21,10 +33,12 @@ function main()
                 # println("passed")
                 i += 1
                 push!(selected_images, randimage)
-                cp(joinpath(randimageinfo[1], randimage), joinpath("/Users/jakeireland/projects/GorillaSCExperiment/data/chosen_object_images_2", randimage))
+                cp(joinpath(randimageinfo[1], randimage), joinpath("/Users/jakeireland/projects/GorillaSCExperiment/data/chosen_object_images_4", randimage))
             end
         end
     end
+    
+    return nothing
 end
 
 main()
