ce9276144b663c300f93516c0d6560f68273ff00
Jake Ireland <jakewilliami@icloud.com>
2022-10-01 09:04:32 +1300

    Added Will's small changes

--- src/attentional-blink-implicit-task/src/config.ts
+++ src/attentional-blink-implicit-task/src/config.ts
@@ -4,7 +4,7 @@ import {
     ResponseKeyCode,
     ImageUtilsConfigs,
     NumRange
-} from 'types';
+} from './types';
 
 // define proportion of distractors (i.e., 33% of trials)
 // are distractor arrays, so proportion is 0.33
@@ -70,7 +70,7 @@ const imageUtilsConfigs: ImageUtilsConfigs = {
 // Main experiment configs struct
 export const exprConfigs: ExperimentConfigs = {
     // Trial/block size definitions
-    nT2ImagesPerBlock: 50, //This was previously 60
+    nT2ImagesPerBlock: 50,
     nImagesInSequence: 20,
     nDistractors: 500,
     nBlocks: 3,
